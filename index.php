e32deac3d0ce1ce860034151f550606f6c1f400f
Martin Pavlík <martin_pavlik@volny.cz>
2025-12-23 08:53:49 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,209 @@
+<html>
+
+<head>
+  <style>
+    table {
+      border-collapse: collapse;
+    }
+    th, td {
+      border: 1px solid black;
+      padding: 8px;
+      text-align: center;
+    }
+    
+    .bad{
+      color: red;
+    }
+    
+    .good{
+      color: green;
+    }
+  }
+  </style>
+</head>
+
+<?php
+
+ini_set('display_errors', '1');
+
+
+function compareDateTimes(DateTime $dateTime1, DateTime $dateTime2): int{
+  $timestamp1 = $dateTime1->getTimeStamp();
+  $timestamp2 = $dateTime2->getTimeStamp();
+  
+  if($timestamp1 == $timestamp2)
+    return 0;
+  
+  return ($timestamp1 < $timestamp2) ? -1 : 1;  
+}
+
+
+function getDateString(DateTime $dateTime): string{
+  return $dateTime->format("Y-m-d");
+}
+
+
+function getCzechDateWithWeekdayString(DateTime $dt): string{
+  $weekdays = [
+    1 => 'pondělí',
+    2 => 'úterý',
+    3 => 'středa',
+    4 => 'čtvrtek',
+    5 => 'pátek',
+    6 => 'sobota',
+    7 => 'neděle',
+  ];
+
+  $weekday = $weekdays[(int)$dt->format('N')];
+
+  return sprintf(
+    '%s %d.%d.%d',
+    $weekday,
+    (int)$dt->format('j'),
+    (int)$dt->format('n'),
+    (int)$dt->format('Y')
+  );
+}
+
+
+function getTimeInMessage($time, $regularTime, $tillFrom){
+  $comparisonResult = compareDateTimes($time, $regularTime);
+  $lsResult = "";
+  
+  if($comparisonResult == -1){
+    $lsResult = "<span class=\"" . (($tillFrom == "FROM") ? "bad\">již" : "good\">jen") . " ";
+  }
+  
+  if($comparisonResult == 1){
+    $lsResult = "<span class=\"" . (($tillFrom == "FROM") ? "good\"" : "bad\"") . ">až ";
+  }
+  
+  $lsResult .= ($tillFrom == "FROM") ? "od " : "do ";
+  
+  $lsResult .= getTimeString($time);
+  
+  if($comparisonResult){
+    $lsResult .= "</span>";
+  }
+  
+  return $lsResult;
+}
+
+
+function getTimeString($dateTime){
+  return $dateTime->format("G:i");
+}
+
+
+class MonthShiftsListRecord{
+  var $_date;
+  var $in;
+  var $order;
+  var $out;
+  var $personId;
+  var $personName;
+  var $regularIn;
+  var $regularOut;
+  var $shiftMessage;
+  
+  
+	public function __construct($input_array){
+		$this->_date = DateTime::createFromFormat('Y-m-d H:i:s', ($input_array[0] . " 00:00:00"), new DateTimeZone('UTC'));
+    $this->order = $input_array[1];
+    $this->personId = $input_array[4];
+    $this->personName = $input_array[6];
+    $this->in = new DateTime("1970-01-01 " . $input_array[9] . ":00", new DateTimeZone('UTC'));
+    $this->out = new DateTime("1970-01-01 " . $input_array[10] . ":00", new DateTimeZone('UTC'));
+    $this->regularIn = new DateTime("1970-01-01 " . $input_array[17] . ":00", new DateTimeZone('UTC'));
+    $this->regularOut = new DateTime("1970-01-01 " . $input_array[18] . ":00", new DateTimeZone('UTC'));
+    $this->shiftMessage = $this->getShiftMessage();
+  }
+  
+  
+  public function getCzechOrder(): string{
+    switch($this->order){
+      case 1: return "dlouhá";
+      case 2: return "krátká";
+      case 3: return "noční";
+    }
+  }
+  
+  
+  public function getTR(){
+    return "<tr><td>" . getCzechDateWithWeekdayString($this->_date) . "</td><td>" . $this->getCzechOrder() . "</td><td>" . $this->getShiftMessage() . "</td></tr>";
+  }
+  
+  
+  public function getKey(){
+    return getDateString($this->_date) . "\\" . $this->order;
+  }
+  
+  
+  public function getShiftMessage(){
+     return getTimeInMessage($this->in, $this->regularIn, "FROM") . " a " . getTimeInMessage($this->out, $this->regularOut, "TILL");
+  }
+    
+  
+  public function isPerson($personName){
+    return $this->personName == $personName;
+  } 
+ 
+}
+
+
+class MonthShiftsList{
+  var $records = array();
+  
+  
+  public function __construct($arrayMapFromGoogleSheet){
+    $i = 0;
+    
+    foreach($arrayMapFromGoogleSheet as $record){
+      if($i){
+        $monthShiftsListRecord = new MonthShiftsListRecord($record);
+        //array_push($this->records, $monthShiftsListRecord);
+        $this->records[$monthShiftsListRecord->getKey()] = $monthShiftsListRecord;
+      }
+      $i++;
+    }
+  }
+  
+  public function getTable(){
+    $ret = "<table>";
+    
+    foreach($this->records as $record){
+      $ret .= $record->getTR();
+    }
+    
+    $ret .= "</table>";
+    
+    return $ret;
+  }
+  
+  
+  public function limitToPersonByName($personName){
+    foreach ($this->records as $record){
+      if(!$record->isPerson($personName))
+        unset($this->records[$record->getKey()]);
+    }
+  }
+  
+}
+
+
+$url = 'https://docs.google.com/spreadsheets/d/1ysbi-0T4SiMJxXUC3TZRgq263Q7QJO73RvLUdl3s1Lk/export?format=csv&gid=303224713';
+
+$rows = array_map('str_getcsv', file($url));
+
+$monthShiftsList = new MonthShiftsList($rows);
+$monthShiftsList->limitToPersonByName("Kuba");
+
+//print_r($monthShiftsList);
+
+echo $monthShiftsList->getTable();
+
+//print_r($rows);
+
+
+?>
+</html>
